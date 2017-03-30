fe9291816c325699de15d61328eedc80839e2cef
S. Merzlyakov <s.merzlyakov@voskhod.ru>
2017-03-30 15:24:59 +0300

    добавил файл конфигурации и начал подготовку к работе с yandex.api

--- modules/sources/yandex.py
+++ modules/sources/yandex.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
+import re
+from mako.template import Template
 
 #
 #
@@ -8,7 +10,40 @@
 #
 #
 
-def loadData():
+YANDEX_XML_TEMPLATE=Template(r"""<?xml version="1.0" encoding="utf-8"?>
+<request>
+<!--Группирующий тег-->
+   <query>
+   <!--Текст поискового запроса-->
+   ${query}
+   </query>
+   <sortby>
+   <!--Тип сортировки результатов поиска-->
+   </sortby>
+   <groupings>
+   <!--Параметры группировки в дочерних тегах-->
+      <groupby attr="d" mode="deep" groups-on-page="10" docs-in-group="1" />
+   </groupings>
+   <page>
+   <!--Номер запрашиваемой страницы результатов поиска-->
+   </page>
+</request>""", input_encoding='utf-8',
+        output_encoding='utf-8',
+        default_filters=['decode.utf8'])
+
+
+def createQuery(keyWords):
+    # убираем точку с конца
+    query=re.sub(r"\.\s*$",r"",keyWords)
+    # заменяем последовательные пробелы на один
+    query=re.sub(r"\s+",r" ",query)
+    # заменяем пробелы перед и за запятой вместе с самой запятую на '|' 
+    query=re.sub(r"\s?,\s?",r"|",query)
+    return query 
+
+def loadData(config):
+    keyWords=config.get('KeyWords','keyWords')
+    print(YANDEX_XML_TEMPLATE.render_unicode(query=createQuery(keyWords)))
     result = [{'header':'Услуги механической обработки металла на заказ...', 'reference':'http://presslit.ru/tooling/',
      'description':'Токарно-фрезерные, расточные, шлифовальные работы. Обработка давлением. ... «ПРЕССЛИТМАШ» изготавливает детали из металла по вашим образцам, эскизам или чертежам.'}]
     return result
--- parser.py
+++ parser.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python
 
 import imp,os
+import ConfigParser
 
-sources=[]
 
-def __load_all_sources(dir="modules/sources"):
+
+
+
+def __load_all_sources(config,dir='modules/sources'):
+    sources =[]
     list_modules=os.listdir(dir)
     list_modules.remove('__init__.py')
     for module_name in list_modules:
         if module_name.split('.')[-1]=='py':
             foo = imp.load_source('module', dir+os.sep+module_name)
             sources.append(foo)
+    return sources
 
 if __name__=='__main__':
-    __load_all_sources()
+    config = ConfigParser.RawConfigParser()
+    config.read('./parser.cfg')
+    sources = __load_all_sources(config)
+
+    # header reference description contacts
     result = []
     for source in sources:
-        result.extend(source.loadData())
+        result.extend(source.loadData(config))
 
     print (result)
     
